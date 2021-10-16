4832c5782d5a93c12150e5a787b80a95114aaf1c
tassioFront <tassioFront@users.noreply.github.com>
2021-10-16 21:36:43 +0000

    deploy: d8c06b0b00510903733606916cc38d4336aae46f

--- _nuxt/static/1634419827/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhello-tj-front\u002F_nuxt\u002Fstatic\u002F1634419827",layout:"blog",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhello-tj-front\u002F",assetsPath:"\u002Fhello-tj-front\u002F_nuxt\u002F",cdnURL:a}}}}(null));
